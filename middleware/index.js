9ebfb268fad74c95a8d78336d02ffe1e22f1e03a
Nestor <ncervan87@gmail.com>
2020-09-02 17:23:55 -0700

    mvp

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,5 @@
+const user = require('../users/userDb')
+
 function logger() {
     return function (req, res, next) {
         console.log(`Client made a ${req.method} request at ${req.url} at ${new Date().toTimeString()}`)
@@ -5,32 +7,71 @@ function logger() {
     }
 }
 
-//validate if req.body & req.body.name are included
-//cannot add user with same name twice
 function validateUserId(req, res, next) {
+    user.getById(req.params.id)
+        .then(response => {
+            if (!response) {
+                console.log('user NOT found with that id')
+                res.status(404).json({ message: 'user not found' })
+            } else {
+                console.log("user found")
+                next()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+function validateUser(req, res, next) {
+    console.log(req.body)
     if (Object.keys(req.body).length === 0) {
+        console.log('no body')
         res.status(400).json({ message: 'missing user data' })
     } else if (!req.body.name) {
+        console.log('no name field')
         res.status(400).json({ message: 'missing name field' })
     } else {
-        req.user = req.body
+        console.log('validate user passed,next')
         next()
     }
 }
+
+//validate if req.body & req.body.name are included
+//cannot add user with same name twice
+// function validateUserId(req, res, next) {
+//     if (Object.keys(req.body).length === 0) {
+//         res.status(400).json({ message: 'missing user data' })
+//     } else if (!req.body.name) {
+//         res.status(400).json({ message: 'missing name field' })
+//     } else {
+//         req.user = req.body
+//         next()
+//     }
+// }
 
 function validatePost(req, res, next) {
     if (Object.keys(req.body).length === 0) {
+        console.log('missing data completely')
         res.status(400).json({ message: 'missing post data' })
     } else if (!req.body.text) {
+        console.log('missing text field')
         res.status(400).json({ message: 'missing text field' })
     } else {
-        req.post = req.body
+        console.log('passed middleware, next')
         next()
     }
 }
 
+// module.exports = {
+//     logger,
+//     validateUserId,
+//     validatePost
+// }
+
 module.exports = {
-    logger,
     validateUserId,
-    validatePost
+    validateUser,
+    validatePost,
+    logger
 }
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const postsDb = require('./posts/postDb')
 const usersDb = require('./users/userDb')
 
-const { logger, validateUserId, validatePost } = require('./middleware')
+// const { logger, validateUserId, validatePost } = require('./middleware')
+const { validateUserId, validateUser, validatePost, logger } = require('./middleware/index')
 
 const server = express();
 
@@ -13,7 +14,7 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-
+//-----------user end points
 server.get('/api/users', (req, res) => {
   usersDb.get()
     .then(response => {
@@ -21,36 +22,6 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
       console.log(err)
-    })
-})
-
-server.get('/api/posts', (req, res) => {
-  postsDb.get()
-    .then(response => {
-      res.status(200).json({ data: response })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})
-
-server.get('/api/users/:id', (req, res) => {
-  usersDb.getById(req.user || req.params.id)
-    .then(response => {
-      res.status(200).json(response)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
-
-server.get('/api/posts/:id', (req, res) => {
-  postsDb.getById(req.params.id)
-    .then(response => {
-      res.status(200).json(response)
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'there was an error retrieving the post' })
     })
 })
 
@@ -64,7 +35,73 @@ server.post('/api/users', (req, res) => {
     })
 })
 
-server.post('/api/posts', (req, res) => {
+server.get('/api/users/:id', validateUserId, (req, res) => {
+  usersDb.getById(req.params.id)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+server.get('/api/users/:id/posts', validateUserId, (req, res) => {
+  usersDb.getUserPosts(req.params.id)
+    .then(response => {
+      res.status(201).json(response)
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'error, could not retrieve user at this time' })
+    })
+})
+
+//returns 0 if no match by id
+server.put('/api/users/:id', validateUserId, validateUser, (req, res) => {
+  const id = req.params.id
+  usersDb.update(id, req.body)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+server.delete('/api/users/:id', validateUserId, (req, res) => {
+  usersDb.remove(req.params.id)
+    .then(response => {
+      res.status(201).json({ message: 'successfully deleted user' })
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'there was a problem deleting user' })
+    })
+})
+
+
+//posts endpoints--------------------------------------
+
+server.get('/api/posts', (req, res) => {
+  postsDb.get()
+    .then(response => {
+      res.status(200).json({ data: response })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+
+server.get('/api/posts/:id', (req, res) => {
+  postsDb.getById(req.params.id)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'there was an error retrieving the post' })
+    })
+})
+
+server.post('/api/posts', validatePost, (req, res) => {
   console.log(req.body)
   postsDb.insert(req.body)
     .then(response => {
@@ -75,17 +112,6 @@ server.post('/api/posts', (req, res) => {
     })
 })
 
-//returns 0 if no match by id
-server.put('/api/users/:id', (req, res) => {
-  const id = req.params.id
-  usersDb.update(id, req.body)
-    .then(response => {
-      res.status(200).json(response)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
 
 //returns 0 if no post was found by id
 server.put('/api/posts/:id', (req, res) => {
@@ -99,15 +125,6 @@ server.put('/api/posts/:id', (req, res) => {
     })
 })
 
-server.delete('/api/users/:id', (req, res) => {
-  usersDb.remove(req.params.id)
-    .then(response => {
-      res.status(201).json({ message: 'successfully deleted user' })
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'there was a problem deleting user' })
-    })
-})
 
 server.delete('/api/posts/:id', (req, res) => {
   postsDb.remove(req.params.id)
@@ -118,5 +135,6 @@ server.delete('/api/posts/:id', (req, res) => {
       res.status(500).json({ message: 'there was a problem deleting the post' })
     })
 })
+
 
 module.exports = server;
